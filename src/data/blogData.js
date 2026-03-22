5ba516c594c80972e6eba21734a437149422abf0
Dawn <dawn.fintech22@gmail.com>
2026-03-22 17:40:37 +0700

    blog page

---
+++ src/data/blogData.js
@@ -0,0 +1,49 @@
+import blog1 from "../assets/img/blog/d248030196ed5dc3b3d01cf6cd369ef7aff2f296.jpg";
+import blog2 from "../assets/img/blog/8dee6dec4190307dc6c7273c0bbf5086605997e4.jpg";
+import blog3 from "../assets/img/blog/56f76fe241417cd682c30a19eecaf20a549cee89.jpg";
+
+export const blogPosts = [
+  {
+    id: 1,
+    image: blog1,
+    title: "Going all-in with millennial design",
+    date: "14 Oct 2022",
+    author: "Admin",
+    category: "Wood",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis. Cursus mattis molestie a iaculis at erat. Nam at lectus urna duis convallis at maximus elementum."
+  },
+  {
+    id: 2,
+    image: blog2,
+    title: "Exploring new ways of decorating",
+    date: "14 Oct 2022",
+    author: "Admin",
+    category: "Handmade",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis. Cursus mattis molestie a iaculis at erat. Nam at lectus urna duis convallis at maximus elementum."
+  },
+  {
+    id: 3,
+    image: blog3,
+    title: "Handmade pieces that took time to make",
+    date: "14 Oct 2022",
+    author: "Admin",
+    category: "Wood",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis. Cursus mattis molestie a iaculis at erat. Nam at lectus urna duis convallis at maximus elementum."
+  }
+];
+
+export const categories = [
+  { name: "Crafts", count: 2 },
+  { name: "Design", count: 8 },
+  { name: "Handmade", count: 7 },
+  { name: "Interior", count: 1 },
+  { name: "Wood", count: 6 },
+];
+
+export const recentPosts = [
+  { id: 1, thumb: blog1, title: "Going all-in with millennial design", date: "03 Aug 2022" },
+  { id: 2, thumb: blog2, title: "Exploring new ways of decorating", date: "03 Aug 2022" },
+  { id: 3, thumb: blog3, title: "Handmade pieces that took time to make", date: "03 Aug 2022" },
+  { id: 4, thumb: blog1, title: "Modern home in Milan", date: "03 Aug 2022" },
+  { id: 5, thumb: blog2, title: "Colorful office redesign", date: "03 Aug 2022" },
+];
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,5 +1,78 @@
+import PageHero from "../components/pageHero";
+import ServiceFeatures from "../components/serviceFeatures";
+import Pagination from "../components/pagination";
+import { blogPosts, categories, recentPosts } from "../data/blogData";
+import searchIcon from "../assets/img/header/akar-icons_search.svg";
+
+const BlogCard = ({ post }) => (
+  <div className="blog-card">
+    <div className="blog-card__image">
+      <img src={post.image} alt={post.title} />
+    </div>
+    <div className="blog-card__meta">
+      <span><i className="fa-solid fa-user"></i> {post.author}</span>
+      <span><i className="fa-solid fa-calendar"></i> {post.date}</span>
+      <span><i className="fa-solid fa-tag"></i> {post.category}</span>
+    </div>
+    <h2 className="blog-card__title">{post.title}</h2>
+    <p className="blog-card__text">{post.summary}</p>
+    <a href="#" className="blog-card__link">Read more</a>
+  </div>
+);
+
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <main className="blog-page">
+      <PageHero title="Blog" currentPage="Blog" />
+
+      <div className="container blog-container">
+        <div className="blog-main">
+          <div className="blog-list">
+            {blogPosts.map(post => (
+              <BlogCard key={post.id} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <aside className="blog-sidebar">
+          <div className="sidebar-search">
+            <input type="text" />
+            <img src={searchIcon} alt="Search" className="search-icon" />
+          </div>
+
+          <div className="sidebar-section">
+            <h3 className="section-title">Categories</h3>
+            <ul className="category-list">
+              {categories.map((cat, index) => (
+                <li key={index}>
+                  <span>{cat.name}</span>
+                  <span>{cat.count}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="sidebar-section">
+            <h3 className="section-title">Recent Posts</h3>
+            <div className="recent-posts">
+              {recentPosts.map(post => (
+                <div key={post.id} className="recent-post-item">
+                  <img src={post.thumb} alt={post.title} />
+                  <div className="info">
+                    <h4>{post.title}</h4>
+                    <span>{post.date}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </aside>
+      </div>
+      <Pagination />
+
+      <ServiceFeatures />
+    </main>
+  );
 }
 
 export default Blog;
